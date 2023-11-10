a856f97ee3495601d3e016e036d716b7d02c244f
jorgeUnas <jorgeunas@gmail.com>
2023-11-10 14:34:44 +0000

    Game working with a nice interface!

--- rock-paper-scissors/code.js
+++ rock-paper-scissors/code.js
@@ -2,13 +2,22 @@
 
 // Buttons 
 const images = document.querySelectorAll('img');
-// Div with result
+
+// Divs
 const divResult = document.querySelector('#divResult');
+const scores = document.querySelector('#scores');
+const gameResult = document.querySelector('#gameResult');
 
 //Declare variables 
 
-let computerSelection = ''
-let playerSelection = ''
+let computerSelection = '';
+let playerSelection = '';
+
+
+//count the times everyone wins
+    
+let computerVictoriesNumber = 0;
+let playerVictoriesNumber = 0;
 
 //Get the PC choice 
 
@@ -27,22 +36,11 @@ function getComputerChoice(){
 
 
 
-//count the times everyone wins
-    
-let computerVictoriesNumber = 0;
-let playerVictoriesNumber = 0;
-
-
-
-//document.write(playerSelection);
-
-
-
-
 function playRound(playerSelection, computerSelection){
     
-    //get the user choice 
-/*do{
+
+/* //get the user choice using a prompt
+do{
 
 playerSelection = prompt('Rock, paper or scissors?!').toLowerCase().trim();
 
@@ -52,8 +50,8 @@ alert('invalid option');
 
 }while(playerSelection != 'rock' & playerSelection != 'paper' & playerSelection != 'scissors'); //Validate the user's choice */
 
-//get a computer selection
 
+//get a computer selection
 
 computerSelection = getComputerChoice();
 
@@ -61,25 +59,25 @@ computerSelection = getComputerChoice();
 
     let result = '';
     if (playerSelection === computerSelection){
-        result = 'There is not winner'
+        result = 'There is not winner';
     }else if (playerSelection === 'paper' & computerSelection == 'rock'){
-        playerVictoriesNumber++
-        result = 'You Win! Paper beats Rock';
+        playerVictoriesNumber++;
+        result = 'You Win! Paper beats Rock 😀';
     }else if (playerSelection === 'paper' & computerSelection == 'scissors'){
-        computerVictoriesNumber++
-        result = 'You Losse! Scissors beats Paper';
+        computerVictoriesNumber++;
+        result = 'You Losse! Scissors beats Paper 🙁';
     }else if (playerSelection === 'rock' & computerSelection == 'scissors'){
-        playerVictoriesNumber++
-        result = 'You Win! Rock beats Scissors';
+        playerVictoriesNumber++;
+        result = 'You Win! Rock beats Scissors 😀';
     }else if (playerSelection === 'rock' & computerSelection == 'paper'){
-        computerVictoriesNumber++
-        result = 'You Losse! Paper beats Rock';
+        computerVictoriesNumber++;
+        result = 'You Losse! Paper beats Rock 🙁';
     }else if (playerSelection === 'scissors' & computerSelection == 'paper'){
-        playerVictoriesNumber++
-        result = 'You Win! Scissors beats Paper';
+        playerVictoriesNumber++;
+        result = 'You Win! Scissors beats Paper 😀';
     }else if (playerSelection === 'scissors' & computerSelection == 'rock'){
-        computerVictoriesNumber++
-        result = 'You Losse! Rock beats Scissors';
+        computerVictoriesNumber++;
+        result = 'You Losse! Rock beats Scissors 🙁';
     };
     return result;
 }
@@ -103,25 +101,36 @@ images.forEach((image) => {
     PC_selection.setAttribute('height', '200px');
     /*PC_selection.textContent = `Computer said ${computerSelection}`;*/
     divResult.appendChild(PC_selection);
+    gameResult.innerHTML = '';
+     scores.innerHTML = '';
     
+    
+    
+    //Show the results and  the winner 
+    
+    if (computerVictoriesNumber === 5){
+        scores.innerHTML = `Victories: ${playerVictoriesNumber}, Defeats: ${computerVictoriesNumber}`;
+        gameResult.innerHTML = 'You lost the game 🙁';
+        playerVictoriesNumber = 0;
+        computerVictoriesNumber = 0;
+    }else if(playerVictoriesNumber === 5){
+        scores.innerHTML = `Victories: ${playerVictoriesNumber}, Defeats: ${computerVictoriesNumber}`;        
+        gameResult.innerHTML = 'You won the game 😀';
+        playerVictoriesNumber = 0;
+        computerVictoriesNumber = 0;
+    };
     
   });
 });
 
 
-
-
 /*
-
-//Show the results and  the winner 
-    
 
 //crate a function game()
 function game() {
     // play the round 5 times
    let i = 1;
     while (i <= 5 ) {
-        document.write(`${i} Round: ${playRound(playerSelection, computerSelection)} <br>`);
        
         i++
     }
@@ -135,26 +144,6 @@ function game() {
         }else{
             return `<h2> You losse the game =( </h2>`
         }
-    }
-    //run playround inside this fucntion 
-    // crate a score for every time player wins or pc wins 
+    };
     
-document.write(game())
-document.write(`<br> Player victories: ${playerVictoriesNumber} <br>`);
-document.write(`Computer victories: ${computerVictoriesNumber} <br>`);
-     
-*/
-
-     /*
-     // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.name);
-  });
-});
 */
